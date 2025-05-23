103cc0b5edd82d600955f11ef36fde7f3aae6078
JohnHellewell <john.hellewell123@gmail.com>
2025-05-23 15:43:11 -0600

    modified robot code to connect to WiFi

--- 6050_test/6050_test.ino
+++ 6050_test/6050_test.ino
@@ -1,21 +1,19 @@
-// Basic demo for accelerometer readings from Adafruit MPU6050
-
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <WiFi.h>
 
-Adafruit_MPU6050 mpu;
+//Network credentials
+const char* ssid = "Hellewell";
+const char* password = "mac&cheese";
+
+Adafruit_MPU6050 mpu; //object
 float z_offset = 0;  // Offset to calibrate Z axis
 
 void setup(void) {
   Serial.begin(115200);
-  while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
-
-  Serial.println("Adafruit MPU6050 test!");
-
-  // For example, GPIO4 = SDA, GPIO5 = SCL
-  Wire.begin(1, 0);
+  
+  Wire.begin(1, 0); //GPIO pins for 6050 connection. (SDA, SCL)
   
   // Try to initialize!
   if (!mpu.begin()) {
@@ -26,69 +24,30 @@ void setup(void) {
   }
   Serial.println("MPU6050 Found!");
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
+  //range options: 2_G, 4_G, 8_G, 16_G. Higher range sacrifices accuracy & power usage
+  mpu.setAccelerometerRange(MPU6050_RANGE_4_G); 
+  
+  //range options: 250_DEG, 500_DEG, 1000_DEG, 2000_DEG
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
+  
+  //bandwidth options: 260_HZ, 184_HZ, 94_HZ, 44_HZ, 21_HZ, 10_HZ, 5_HZ
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
+
+  calibrateZ(); //calibrate the Z axis
+
+  connectToWiFi();
+}
+
+void connectToWiFi() {
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-
-  Serial.println("");
-  delay(100);
-
-  calibrateZ();
+  Serial.println("\nWiFi connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void calibrateZ() {
@@ -99,23 +58,18 @@ void calibrateZ() {
   mpu.getEvent(&a, &g, &temp);
 
   float current_z = a.acceleration.z;
-  z_offset = 9.8 - current_z;
+
+  if(current_z > 5.0){
+    z_offset = 10.0 - current_z; //I used 10.0 instead of 9.8 
+  } else {
+    Serial.println("Unable to calibrate");
+  }
 
   Serial.print("Calibration complete. Applied Z offset: ");
   Serial.println(z_offset);
 }
 
 void loop() {
-
-  // Check if Serial input is available
-  if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');
-    input.trim();  // Remove whitespace and newlines
-
-    if (input.equalsIgnoreCase("calibrate")) {
-      calibrateZ();
-    }
-  }
 
   /* Get new sensor events with the readings */
   sensors_event_t a, g, temp;
@@ -133,14 +87,9 @@ void loop() {
   Serial.print(", corrected Z: ");
   Serial.print(z_corrected);
   Serial.println(" m/s^2");
+  Serial.print("Z axis offset: ");
+  Serial.println(z_offset);
 
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
 
   Serial.print("Temperature: ");
   Serial.print(temp.temperature);
