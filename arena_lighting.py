1fec7ad136048bb52c3506471335e9af01c5eb6a
JohnHellewell <john.hellewell123@gmail.com>
2025-08-06 22:42:34 -0600

    first version of light program. It is able to control the lights with OLA. Just a single color

--- arena_lighting.py
+++ arena_lighting.py
@@ -1,24 +1,19 @@
-import serial
+from ola.ClientWrapper import ClientWrapper
 import time
 
-DMX_PORT = 'COM14'  # Your USB-DMX adapter port
-DMX_CHANNELS = 512
+UNIVERSE = 1
 
-# Build DMX frame: 512 channels, CH1 = dimmer, CH2 = red
-dmx_data = bytearray([0] * DMX_CHANNELS)
-dmx_data[0] = 255  # CH1: Master dimmer full
-dmx_data[1] = 255  # CH2: Red full
-# CH3–CH6 (Green, Blue, Amber, White) default to 0
+# Blue: full dimmer, full blue, no red/green
+data = [0, 0, 255, 0] + [0] * 508  # total 512 channels
 
-# Open DMX serial port
-ser = serial.Serial(DMX_PORT, baudrate=250000, bytesize=8, parity='N', stopbits=2)
+def dmx_sent(status):
+    if status.Succeeded():
+        print("DMX data sent successfully!")
+    else:
+        print("Failed to send DMX data")
 
-while True:
-    ser.break_condition = True
-    time.sleep(0.001)  # Break for 88µs or more
-    ser.break_condition = False
-    time.sleep(0.001)  # Mark After Break
+wrapper = ClientWrapper()
+client = wrapper.Client()
+client.SendDmx(UNIVERSE, bytearray(data), dmx_sent)
 
-    ser.write(bytes([0]))        # DMX Start Code = 0
-    ser.write(dmx_data)          # 512 bytes of DMX data
-    time.sleep(0.03)             # Send ~30 times per second
+wrapper.Run()
