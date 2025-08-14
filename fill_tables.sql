8cbd14543562ec02ebfc5c85ae4b8eb8a626ff8b
JohnHellewell <john.hellewell123@gmail.com>
2025-08-14 07:49:09 -0600

    changed weapon limit value

--- fill_tables.sql
+++ fill_tables.sql
@@ -2,7 +2,7 @@ USE ROBOT_CITY;
 
 INSERT INTO robot_type (bot_type, steering_limit, forward_limit, weapon_limit, bidirectional_weapon)
 VALUES  ('DRUM', 0.6, 1.0, 0.4, 1),
-        ('HORIZONTAL', 0.6, 1.0, 0.4, 0),
+        ('HORIZONTAL', 0.6, 1.0, 0.2, 0),
         ('VERTICAL', 0.3, 0.4, 0.4, 1),
         ('LIFTER', 1.0, 1.0, 1.0, 0);
 
@@ -11,5 +11,5 @@ VALUES  (10, "192.168.8.10", 4210, "DRUM", "YELLOW", 0, 1, 1, 0),
         (11, "192.168.8.11", 4211, "DRUM", "BLUE", 0, 1, 1, 0),
         (12, "192.168.8.12", 4212, "DRUM", "ORANGE", 0, 1, 1, 0),
         (13, "192.168.8.13", 4213, "DRUM", "GREEN", 0, 1, 1, 0),
-        (20, "192.168.8.20", 4220, "HORIZONTAL", "YELLOW", 0, 0, 0, 0), 
+        (20, "192.168.8.20", 4220, "HORIZONTAL", "YELLOW", 0, 1, 0, 0), 
         (30, "192.168.8.30", 4230, "VERTICAL", "YELLOW", 0, 0, 0, 0);   
