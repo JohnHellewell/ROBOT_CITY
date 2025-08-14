33a24bff4874368bbf0804ff16be822227b480a8
JohnHellewell <john.hellewell123@gmail.com>
2025-08-14 06:15:51 -0600

    removed comments that were causing errors

--- fill_tables.sql
+++ fill_tables.sql
@@ -5,5 +5,5 @@ VALUES  (10, "192.168.8.10", 4210, "DRUM", "YELLOW", 0, 1, 1, 0),
         (11, "192.168.8.11", 4211, "DRUM", "BLUE", 0, 1, 1, 0),
         (12, "192.168.8.12", 4212, "DRUM", "ORANGE", 0, 1, 1, 0),
         (13, "192.168.8.13", 4213, "DRUM", "GREEN", 0, 1, 1, 0),
-        (20, "192.168.8.20", 4220, "HORIZONTAL", "YELLOW", 0, 0, 0, 0), --not sure about inverse values
-        (30, "192.168.8.30", 4230, "VERTICAL", "YELLOW", 0, 0, 0, 0);   --not sure about inverse values
+        (20, "192.168.8.20", 4220, "HORIZONTAL", "YELLOW", 0, 0, 0, 0), 
+        (30, "192.168.8.30", 4230, "VERTICAL", "YELLOW", 0, 0, 0, 0);   
