ce3955861f268c69d35a36aa109b21b6f335f61c
JohnHellewell <john.hellewell123@gmail.com>
2025-08-14 06:50:51 -0600

    fixed small error in MYSQL

--- fill_tables.sql
+++ fill_tables.sql
@@ -6,7 +6,7 @@ VALUES  ('DRUM', 0.6, 1.0, 0.4, 1),
         ('VERTICAL', 0.3, 0.4, 0.4, 1),
         ('LIFTER', 1.0, 1.0, 1.0, 0);
 
-INSERT INTO Robot (robot_id, local_ip, network_port, robot_type, color, CH1_INVERT, CH2_INVERT, CH3_INVERT, INVERT_DRIVE)
+INSERT INTO robot (robot_id, local_ip, network_port, robot_type, color, CH1_INVERT, CH2_INVERT, CH3_INVERT, INVERT_DRIVE)
 VALUES  (10, "192.168.8.10", 4210, "DRUM", "YELLOW", 0, 1, 1, 0),
         (11, "192.168.8.11", 4211, "DRUM", "BLUE", 0, 1, 1, 0),
         (12, "192.168.8.12", 4212, "DRUM", "ORANGE", 0, 1, 1, 0),
